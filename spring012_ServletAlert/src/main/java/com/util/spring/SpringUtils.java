9b274f5eb9ea1b1fc425ec992e2bb84bcd8104b1
chan0yeol <oco723@naver.com>
2025-01-20 10:09:16 +0900

    spring011_SPA에 필요한 alert 라이브러리 생성

---
+++ spring012_ServletAlert/src/main/java/com/util/spring/SpringUtils.java
@@ -0,0 +1,33 @@
+package com.util.spring;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Spring Framework의 SPA(Single Page Application)개발을 위한 라이브러리
+ * Controller 에서 javascript를 통한 Alert 이동 흐름 제어  
+ * @author 오찬열
+ * @since 2025.01.20
+ * @version 1.0
+ * 
+ */
+public class SpringUtils {
+	
+	/**
+	 * HttpServletResponse에 이동하는 URL과 msg를 입력받아 javascript alert을 동작
+	 * @param response javax.servlet.HttpServletResponse 브라우저에 응답해주는 객체
+	 * @param msg Alert의 메세지
+	 * @param url 이동흐름 요청 경로
+	 * @throws IOException
+	 */
+	public static void responseAlert(HttpServletResponse response,
+										String msg,
+										String url) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.printf("<script> alert(%s); location.href='./%s';</script>", msg, url);
+		out.flush();
+	}
+}
